f3b18f0c776d81431eef5d307d7c871b5726f762
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-10 11:37:27 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Isklucheniya
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var i = 5;
+                var j = i / 0;
+                Console.WriteLine(j);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Работа завершена");
+            }
+            
+            while (true)
+            {
+                var input = Console.ReadLine();
+                
+                //проверяем ввод на интовое значение
+                if (int.TryParse(input, out int result))
+                {
+                    Console.WriteLine($"Интовый {result}");
+                }
+                else
+                {
+                    Console.WriteLine("некорректный ввод");
+                }
+            }
+
+        }
+    }
+}
